9ab9e989e8b2bc0049b198bfc7609c370c4c8ad3
mtpatil10 <89735662+mtpatil10@users.noreply.github.com>
2021-08-29 19:33:53 +0530

    Add files via upload

---
+++ Lecture 3 and 4-Variables in python.py
@@ -0,0 +1,19 @@
+print('navin\'s "laptop"')  # \ directs to ignore '
+
+print('navin'+ '  navin')
+
+print(r'docs\navin') # r which is a row string directs to ignore special meaning of \n
+
+print()  # for empty line
+
+name='youtube'  # y  o  u  t  u  b  e
+                # 0  1  2  3  4  5  6
+                #-7 -6 -5 -4 -3 -2 -1
+print(name)
+print(name[0])
+print(name[-1])
+print(name[1:])
+print(name[:4])
+print('my'+name[3:7])
+print(len(name))
+
